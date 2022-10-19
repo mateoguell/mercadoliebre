a27f015177d73866fb3c1bad93f16a96195a2b8c
mateoguell <m4t30guell@gmail.com>
2022-10-18 22:26:04 -0300

    tp entregado v2

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+app.listen(process.env.PORT || 3000, () => console.log('El servidor está corriendo en el puerto 3000'));
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
